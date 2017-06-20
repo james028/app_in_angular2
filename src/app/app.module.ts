d2c10dd0211147a869b494a290ca5628652a4aa3
james028 <kubak.15@gmail.com>
2017-06-20 17:12:35 +0200

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CommentComponent } from './comment.component';
+import { CommentService } from './comment-service';
+import { DataService } from './data.service';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    CommentService,
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
